0b8d62cebe7ea2c2268ea00add0656631933aab6
Andrea Rus <andrearus27@gmail.com>
2021-04-02 19:54:53 +0100

    name cases - upper, lower, title method

--- hello_world.py
+++ hello_world.py
@@ -6,8 +6,20 @@ print(message)
 message = "Hello python crash course world"
 print(message)
 
-# strings names_cases.py
+# strings
+# 2-3 personal message
 
 first_name = "sarah"
 message = f"Hello {first_name.title()}, would you like to learn some Python today?"
 print(message)
+
+# 2-4 name cases
+
+last_name = "russell"
+lower_case = last_name.lower()
+upper_case = last_name.upper()
+title_method = last_name.title()
+
+print(upper_case)
+
+# 2-5 famous quotes
