18a7a791e9320ab9f884b5a25e89c909eb830f29
Andrea Rus <andrearus27@gmail.com>
2021-04-11 18:21:48 +0100

    function
    function that can be used with other variables that take a user input, rather than number

--- hello_world.py
+++ hello_world.py
@@ -340,8 +340,17 @@ print('World')
 
 
 def subtotal(orderAmount, salesTax):
-    subtotal = float(orderAmt) * (1 + float(salesTax))
+    subtotal = float(orderAmount) * (1 + float(salesTax))
     return subtotal
 
 
 firstOrderTotal = subtotal(300, .08)
+secondOrderTotal = subtotal(400, .06)
+
+thirdOrderAmount = input("What was the order amount? ")
+thirdTax = input("Enter your sales tax rate")
+thirdOrderTotal = subtotal(thirdOrderAmount, thirdTax)
+
+print("Your subtotal for the first order is %.2f" % firstOrderTotal)
+print("Your subtotal for the second order is %.2f" % secondOrderTotal)
+print("Your subtotal for the third order is %.2f" % thirdOrderTotal)
