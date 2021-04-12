f6157ac9c4121acfaefe000cf3e2104020c1aa74
Andrea Rus <andrearus27@gmail.com>
2021-04-12 12:35:00 +0100

    pass keyword

--- hello_world.py
+++ hello_world.py
@@ -362,13 +362,19 @@ def orderMsg():
 # orderMsg()
 
 
-# function
+# === function
 
 
 def fullName(firstName, lastName):
     print(f"{lastName}, {firstName}")
-    print("Initials:" + firstName[0] + lastName[0])
+    print("Initials: " + firstName[0] + lastName[0])
     return
 
 
+def personInfo():
+    pass  # need to know where to get this info
+
+
 fullName("Mighty", "Mouse")
+
+# pass keyword - placeholder for something we plan to define later
