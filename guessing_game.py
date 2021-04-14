7d3cf4b7ac140250abc2c39520586be44b7270e1
Andrea Rus <andrearus27@gmail.com>
2021-04-14 14:03:37 +0100

    generate secret number with randit function

--- guessing_game.py
+++ guessing_game.py
@@ -1,7 +1,19 @@
+# This is a guess the number game
 import random
 
-name = input("What is your name? ")
-print(f"Well, {name} , I am thinking of a number between 1 and 20. ")
+print("What is your name? ")
+name = input()
 
-guess = input("Take a guess")
-if i is range(0, 21):
+print(f"Well, {name}, I am thinking of a number between 1 and 20. ")
+secretNumber = random.randint(1, 20)
+
+for guessesTaken in range(1, 7):
+    print("Take a guess.")
+    guess = int(input())
+
+    if guess < secretNumber:
+        print("Your guess is too low.")
+    elif guess > secretNumber:
+        print("Your guess is too high.")
+    else:
+        break  # This condition is for the correct guess
