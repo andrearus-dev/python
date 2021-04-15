6a4a1cf6141476bc84debe2f447f0ff1fd5ba7ba
Andrea Rus <andrearus27@gmail.com>
2021-04-15 13:02:13 +0100

    variables

--- snake_game.py
+++ snake_game.py
@@ -1,3 +1,12 @@
+# Building snake game
 from turtle import *
 from random import randrange
 from freegames import square.vector
+
+# Variables - food, snake and aim
+
+food = vector(0, 0)
+snake = vector(10, 0)
+aim = vector(0, -10)
+
+# 3 Functions - Change, Inside and Move
