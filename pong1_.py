99de3f6de933f4137080de906125b6fff5dfe7a8
Andrea Rus <andrearus27@gmail.com>
2021-04-16 15:55:24 +0100

    pong ball

--- pong1_.py
+++ pong1_.py
@@ -28,6 +28,15 @@ paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()  # turtles usually draw a line as they're moving but we don't want that
 paddle_a.goto(350, 0)
 
+# Ball
+
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("white")
+ball.shapesize(stretch_wid=1, stretch_len=1)
+ball.penup()
+ball.goto(0, 0)
 
 # Main game loop
 
