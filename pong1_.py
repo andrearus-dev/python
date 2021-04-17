38459f356813fbb4f23354e034b981a301ce8858
Andrea Rus <andrearus27@gmail.com>
2021-04-17 09:32:42 +0100

    Update pong1_.py

--- pong1_.py
+++ pong1_.py
@@ -102,4 +102,6 @@ while True:
 
 # Paddle and ball collisions
 
-if ball.xcor() > 340 and ball.xcor() < 350 and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+    ball.setx(340)
+    ball.dx *= -1
