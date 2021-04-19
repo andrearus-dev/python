9d3e637d7887d5aa7385e9e9c3f0c43b7b2779c2
Andrea Rus <andrearus27@gmail.com>
2021-04-19 14:29:27 +0100

    Update pong1_.py

--- pong1_.py
+++ pong1_.py
@@ -92,21 +92,21 @@ elif ball.ycor() < -290:
     ball.sety(-290)
     ball.dy *= -1
 
-if ball.xcor() < 390:
+if ball.xcor() < 350:
     ball.goto(0, 0)
     ball.dx *= -1
 
-elif ball.xcor() < -390:
+elif ball.xcor() < -350:
     ball.goto(0, 0)
     ball.dx *= -1
 
 # Paddle and ball collisions
 
-if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
-    ball.setx(340)
+if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
     ball.dx *= -1
+    os.system("afplay bounce.wav&")
 
 
-if (ball.xcor() > -340 and ball.xcor() < -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
-    ball.setx(340)
+elif ball.xcor() > 340 and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
     ball.dx *= -1
+    os.system("afplay bounce.wav&")
