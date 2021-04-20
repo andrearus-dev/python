5ad830edbbb9bfbff411bfd1cfa2e4995e877c64
Andrea Rus <andrearus27@gmail.com>
2021-04-20 18:37:28 +0100

    Update snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -10,4 +10,9 @@ from tkinter import messagebox
 class cube(object):
     rows = 0
     w = 0
+
     def __init__(self, start, dirnx=1, dirny=0, color=(255, 0, 0))
+    pass
+
+    def draw(self, surface, eyes=False):
+        pass
