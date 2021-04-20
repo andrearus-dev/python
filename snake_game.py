2f3353bbd66d72080a7088f936131abac2949fef
Andrea Rus <andrearus27@gmail.com>
2021-04-20 18:35:43 +0100

    first object

--- snake_game.py
+++ snake_game.py
@@ -3,3 +3,11 @@ import random
 import pygame
 import tkinter as tk
 from tkinter import messagebox
+
+
+# two objects
+
+class cube(object):
+    rows = 0
+    w = 0
+    def __init__(self, start, dirnx=1, dirny=0, color=(255, 0, 0))
