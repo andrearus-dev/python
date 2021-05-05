3d2c266a63593a40f200bed52c1aa6cca171095c
Andrea Rus <andrearus27@gmail.com>
2021-05-05 14:24:06 +0100

    sentiment analysis
    getting polarity score of the text

--- news.py
+++ news.py
@@ -21,6 +21,14 @@ article.parse()  # dissects article in the parts that it needs
 article.nlp()
 
 print(f'Title: {article.title}')
-print(f'Author: {article.author}')
+print(f'Author: {article.authors}')
 print(f'Publication Date: {article.publish_date}')
 print(f'Summary: {article.summary}')
+
+# Sentiment analysis
+
+analysis = TextBlob(article.text)
+print(analysis.polarity)
+print(f'Sentiment: {'positive' if analysis.polarity > 0 else 'negative' if analysis.polarity < 0 else 'neutral'}')
+
+# Building the graphical user interface
