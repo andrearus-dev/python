2061444ef756f6e43564101327300000a36dbe76
Andrea Rus <andrearus27@gmail.com>
2021-05-05 14:52:22 +0100

    title GUI

--- news.py
+++ news.py
@@ -5,32 +5,32 @@ from textblob import TextBlob  # for sentiment analysis
 import nltk
 import tkinter as tk
 
-nltk.download('punkt')
+# nltk.download('punkt')
 
-# get summary of article = pass in url
+# # get summary of article = pass in url
 
-url = 'https://techcrunch.com/2021/05/05/cymulate-nabs-45m-to-test-and-improve-cybersecurity-defenses-via-attack-simulations/'
-
-
-# create article object from newspaper library
-
-article = Article(url)
-article.download()  # download the data
-article.parse()  # dissects article in the parts that it needs
-
-article.nlp()
-
-print(f'Title: {article.title}')
-print(f'Author: {article.authors}')
-print(f'Publication Date: {article.publish_date}')
-print(f'Summary: {article.summary}')
+# url = 'https://techcrunch.com/2021/05/05/cymulate-nabs-45m-to-test-and-improve-cybersecurity-defenses-via-attack-simulations/'
 
 
-# Sentiment analysis
+# # create article object from newspaper library
 
-analysis = TextBlob(article.text)
-print(analysis.polarity)
-print(f'Sentiment: {"positive" if analysis.polarity > 0 else "negative" if analysis.polarity < 0 else "neutral"}')
+# article = Article(url)
+# article.download()  # download the data
+# article.parse()  # dissects article in the parts that it needs
+
+# article.nlp()
+
+# print(f'Title: {article.title}')
+# print(f'Author: {article.authors}')
+# print(f'Publication Date: {article.publish_date}')
+# print(f'Summary: {article.summary}')
+
+
+# # Sentiment analysis
+
+# analysis = TextBlob(article.text)
+# print(analysis.polarity)
+# print(f'Sentiment: {"positive" if analysis.polarity > 0 else "negative" if analysis.polarity < 0 else "neutral"}')
 
 
 # Building the graphical user interface
@@ -39,5 +39,15 @@ print(f'Sentiment: {"positive" if analysis.polarity > 0 else "negative" if analy
 root = tk.Tk()
 root.title("News Summarizer")
 root.geometry('1200x600')
+
+# create individual buttons/ labels/ textboxes
+
+tlabel = tk.Label(root, text="Title")
+tlabel.pack()  # to see it on the screen
+
+title = tk.Text(root, height=1, width=140)
+# state disabled so the user can't go in and change it
+title.config(state='disabled', bg='#dddddd')
+title.pack()
 
 root.mainloop()
