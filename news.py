536c641a776cb05eca8adb5355761d591f855af2
Andrea Rus <andrearus27@gmail.com>
2021-05-05 13:59:29 +0100

    article object

--- news.py
+++ news.py
@@ -1,4 +1,17 @@
-import tkinter as tk
-import nltk
+# libraries
+
+from newspaper import Article
 from textblob import TextBlob
-from newspaper import article
+import nltk
+import tkinter as tk
+
+# get summary of article = pass in url
+
+url = 'https://techcrunch.com/2021/05/05/cymulate-nabs-45m-to-test-and-improve-cybersecurity-defenses-via-attack-simulations/'
+
+
+# create article object from newspaper library
+
+article = Article(url)
+article.download()  # download the data
+article.parse()  # dissects article in the parts that it needs
