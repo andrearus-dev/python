2d8381c17f7a823d788293f96dac94f54fd829e1
Andrea Rus <andrearus27@gmail.com>
2021-05-05 15:37:49 +0100

    Update news.py

--- news.py
+++ news.py
@@ -5,37 +5,43 @@ from textblob import TextBlob  # for sentiment analysis
 import nltk
 import tkinter as tk
 
-# nltk.download('punkt')
 
-# # get summary of article = pass in url
+def summarize():
+ # get article from the texbox
+    url = utext.get('1.0', "end")  # need a start and end point
 
-# url = 'https://techcrunch.com/2021/05/05/cymulate-nabs-45m-to-test-and-improve-cybersecurity-defenses-via-attack-simulations/'
+    # create article object from newspaper library
+
+    article = Article(url)
+    article.download()  # download the data
+    article.parse()  # dissects article in the parts that it needs
+
+    article.nlp()
+
+    title.config(state='normal')
+    author.config(state='normal')
+    publication.config(state='normal')
+    summary.config(state='normal')
+    sentiment.config(state='normal')
+    # we need to have the state enabled to be able to edit
+
+    title.config(state='disabled')
+    author.config(state='disabled')
+    publication.config(state='disabled')
+    summary.config(state='disabled')
+    sentiment.config(state='disabled')
+
+    # Sentiment analysis
+
+    analysis = TextBlob(article.text)
+    print(analysis.polarity)
+    print(
+        f'Sentiment: {"positive" if analysis.polarity > 0 else "negative" if analysis.polarity < 0 else "neutral"}')
+
+    # Building the graphical user interface
 
 
-# # create article object from newspaper library
-
-# article = Article(url)
-# article.download()  # download the data
-# article.parse()  # dissects article in the parts that it needs
-
-# article.nlp()
-
-# print(f'Title: {article.title}')
-# print(f'Author: {article.authors}')
-# print(f'Publication Date: {article.publish_date}')
-# print(f'Summary: {article.summary}')
-
-
-# # Sentiment analysis
-
-# analysis = TextBlob(article.text)
-# print(analysis.polarity)
-# print(f'Sentiment: {"positive" if analysis.polarity > 0 else "negative" if analysis.polarity < 0 else "neutral"}')
-
-
-# Building the graphical user interface
-
-# create tkinter window screen
+    # create tkinter window screen
 root = tk.Tk()
 root.title("News Summarizer")
 root.geometry('1200x600')
@@ -83,7 +89,8 @@ utext = tk.Text(root, height=1, width=140)
 utext.pack()
 
 # Button
-btn = tk.Button(root, text="Summarize")
+# refer to the function, not call it
+btn = tk.Button(root, text="Summarize", command=summarize)
 btn.pack()
 
 
