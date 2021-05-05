c31237bc29e1de02f039ce3d6dfe84ce741ceab1
Andrea Rus <andrearus27@gmail.com>
2021-05-05 17:34:08 +0100

    Update news.py

--- news.py
+++ news.py
@@ -8,7 +8,7 @@ import tkinter as tk
 
 def summarize():
  # get article from the texbox
-    url = utext.get('1.0', "end")  # need a start and end point
+    url = utext.get('1.0', "end").strip()  # need a start and end point
 
     # create article object from newspaper library
 
@@ -18,6 +18,8 @@ def summarize():
 
     article.nlp()
 
+    # change content of individual text boxes
+
     title.config(state='normal')
     author.config(state='normal')
     publication.config(state='normal')
@@ -25,18 +27,32 @@ def summarize():
     sentiment.config(state='normal')
     # we need to have the state enabled to be able to edit
 
+    title.delete('1.0', 'end')
+    title.insert('1.0', article.title)
+
+    author.delete('1.0', 'end')
+    author.insert('1.0', article.authors)
+
+    publication.delete('1.0', 'end')
+    publication.insert('1.0', article.publish_date)
+
+    summary.delete('1.0', 'end')
+    summary.insert('1.0', article.summary)
+
+    # Sentiment analysis
+
+    analysis = TextBlob(article.text)
+    sentiment.delete('1.0', 'end')
+    sentiment.insert(
+        '1.0', f'Polarity: {analysis.polarity}Sentiment: {"positive" if analysis.polarity > 0 else "negative" if analysis.polarity < 0 else "neutral"}')
+
+    # disable configuration so the user can't change content
+
     title.config(state='disabled')
     author.config(state='disabled')
     publication.config(state='disabled')
     summary.config(state='disabled')
     sentiment.config(state='disabled')
-
-    # Sentiment analysis
-
-    analysis = TextBlob(article.text)
-    print(analysis.polarity)
-    print(
-        f'Sentiment: {"positive" if analysis.polarity > 0 else "negative" if analysis.polarity < 0 else "neutral"}')
 
     # Building the graphical user interface
 
@@ -81,8 +97,16 @@ summary = tk.Text(root, height=20, width=140)
 summary.config(state='disabled', bg='#dddddd')
 summary .pack()
 
+# Sentiment Analysis
+selabel = tk.Label(root, text="Summary")
+selabel.pack()
+
+sentiment = tk.Text(root, height=1, width=140)
+sentiment.config(state='disabled', bg='#dddddd')
+sentiment.pack()
+
 # URL
-ulabel = tk.Label(root, text="Sentiment Analysis")
+ulabel = tk.Label(root, text="URL")
 ulabel.pack()
 
 utext = tk.Text(root, height=1, width=140)
