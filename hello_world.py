dd54a45390eccc51f59da175c8f23a10a575ed27
Andrea Rus <andrearus27@gmail.com>
2021-06-16 14:05:52 +0100

    accessing instance attributes

--- hello_world.py
+++ hello_world.py
@@ -546,3 +546,13 @@ class Parrot:
 
 blu = Parrot("Blu", 10)
 woo = Parrot("Woo", 15)
+
+# accessing the class attributes
+
+print("Blu is a {}".format(blu.__class__.species))
+print("Woo is also a {}".format(blu.__class__.species))
+
+# accessing the instance attributes
+
+print("{} is {} years old.".format(blu.name, blu.age))
+print("{} is {} years old").format(woo.name, woo.age)
