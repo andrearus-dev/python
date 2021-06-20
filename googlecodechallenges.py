bfa16bfbfb3db71192ed70144e6e25c39d62f1e9
Andrea Rus <andrearus27@gmail.com>
2021-06-20 18:10:55 +0100

    generating random number of people

---
+++ googlecodechallenges.py
@@ -0,0 +1,9 @@
+# variables
+
+# Write a code fragment (a short part of a Python program) to count heads, shoulders, knees, and toes at a party. The grader will automatically define a variable people for you, counting the number of people at the party. Your code must define four variables, one called heads, one called shoulders, one called knees, and one called toes, equal to the number of heads, shoulders, knees, and toes in total at the party. Your program does not need to print any output. The grader will select a new random value for people each time your code runs.
+
+import random
+random_num = random.randint(0, 100)
+
+people = random_num
+print(people)
