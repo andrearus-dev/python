d5c2b8e82b3c707a03e1a5e9eee86a3b1b6aadae
Andrea Rus <andrearus27@gmail.com>
2021-06-23 12:19:47 +0100

    Update googlecodechallenges.py

--- googlecodechallenges.py
+++ googlecodechallenges.py
@@ -67,3 +67,18 @@ def array123(nums):
 array123([1, 1, 2, 3, 1])
 array123([1, 1, 2, 4, 1])
 array123([1, 1, 2, 1, 2, 3])
+
+
+# Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it. For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24. For the test cases, the range will be between 1 and 18 and the input will always be an integer.
+
+
+def FirstFactorial(num):
+
+    if num == 1:
+        return 1
+
+    else:
+        return num*(FirstFactorial(int((num-1))))
+
+
+print(FirstFactorial(input()))
